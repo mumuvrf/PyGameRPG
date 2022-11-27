de9f8e998660294767fc52dc3ae1ef9cbcdadf5d
Samuel Jabes <samueljcc@al.insper.edu.br>
2022-11-27 17:23:45 -0300

    Adiciona o chão de teste e sprites testes

--- level.py
+++ level.py
@@ -44,10 +44,18 @@ class YSortCameraGroup(pygame.sprite.Group):
         self.half_height = self.display_surface.get_size()[1] // 2
         self.offset = pygame.math.Vector2()
 
+        # Criando o chão (floor)
+        self.floor_surf = pygame.image.load('tilemap/ground.png').convert()
+        self.floor_rect = self.floor_surf.get_rect(topleft = (0, 0))
+
     def custom_draw(self, player):
         # Conseguindo o deslocamento (offset) para a câmera
         self.offset.x = player.rect.centerx - self.half_width
         self.offset.y = player.rect.centery - self.half_height
+
+        # Desenhando o chão
+        floor_offset_pos = self.floor_rect.topleft - self.offset
+        self.display_surface.blit(self.floor_surf, floor_offset_pos)
 
         # Desenhar sprites
         for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
