9ffa3a902aa8e4498840a4a5f88093d06498969b
Samuel Jabes <samueljcc@al.insper.edu.br>
2022-11-28 23:34:34 -0300

    Adicionando mecânica de animação do nosso player

--- player.py
+++ player.py
@@ -1,62 +1,146 @@
 import pygame
 from settings import *
+from support import import_folder
+
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, grupo, obstacle_sprites):
         super().__init__(grupo)
-        self.image = pygame.image.load('assets/player.png').convert_alpha()
-        self.rect = self.image.get_rect(topleft = pos)
+        self.image = pygame.image.load('graphics/test/player.png').convert_alpha()
+        self.rect = self.image.get_rect(topleft=pos)
         self.hitbox = self.rect.inflate(0, -26)
 
+        # gráficos de configuração
+        self.import_player_assets()
+        self.status = 'down'
+        self.frame_index = 0
+        self.animation_speed = 0.15
+
+        # movimento
         self.direction = pygame.math.Vector2()
         self.speed = 5
+        self.attacking = False
+        self.attack_cooldown = 400
+        self.attack_time = None
 
         self.obstacle_sprites = obstacle_sprites
 
+    def import_player_assets(self):
+        character_path = 'graphics/player/'
+        self.animations = {
+            'up': [], 'down': [], 'left': [], 'right': [],
+            'right_idle': [], 'left_idle': [], 'up_idle': [], 'down_idle': [],
+            'right_attack': [], 'left_attack': [], 'up_attack': [], 'down_attack': []
+        }
+
+        for animation in self.animations.keys():
+            full_path = character_path + animation
+            self.animations[animation] = import_folder(full_path)
+
     def input(self):
         keys = pygame.key.get_pressed()
-
-        if keys[pygame.K_UP]:
+        # comandos de movimento (entradas)
+        if keys[pygame.K_w]:
             self.direction.y = -1
-        elif keys[pygame.K_DOWN]:
+            self.status = 'up'
+        elif keys[pygame.K_s]:
             self.direction.y = 1
+            self.status = 'down'
         else:
             self.direction.y = 0
 
-        if keys[pygame.K_RIGHT]:
+        if keys[pygame.K_d]:
             self.direction.x = 1
-        elif keys[pygame.K_LEFT]:
+            self.status = 'right'
+        elif keys[pygame.K_a]:
             self.direction.x = -1
+            self.status = 'left'
         else:
             self.direction.x = 0
-            
-    def move(self,speed):
+
+        # comandos de ataque (entradas)
+        if keys[pygame.K_e] and not self.attacking:
+            self.attacking = True
+            self.attack_time = pygame.time.get_ticks()
+            print('atacando')
+        # comando de magia (entradas)
+        if keys[pygame.K_q] and not self.attacking:
+            self.attacking = True
+            self.attack_time = pygame.time.get_ticks()
+            print('magia')
+
+    def get_status(self):
+
+        # estado ocioso
+        if self.direction.x == 0 and self.direction.y == 0:
+            if not 'idle' in self.status and not 'attack' in self.status:
+                self.status += '_idle'
+
+        if self.attacking:
+            self.direction.x = 0
+            self.direction.y = 0
+            if not 'attack' in self.status:
+                if 'idle' in self.status:
+                    self.status = self.status.replace('_idle', '_attack')
+                else:
+                    self.status += '_attack'
+        else:
+            if 'attack' in self.status:
+                self.status = self.status.replace('_attack', '')
+
+    def move(self, speed):
         if self.direction.magnitude() > 1:
             self.direction.x = 0
             self.direction.y = 0
+
         self.hitbox.x += self.direction.x * speed
         self.collision('horizontal')
         self.hitbox.y += self.direction.y * speed
         self.collision('vertical')
         self.rect.center = self.hitbox.center
 
-    def collision(self,direction):
+    def collision(self, direction):
         if direction == 'horizontal':
             for sprite in self.obstacle_sprites:
                 if sprite.hitbox.colliderect(self.hitbox):
-                    if self.direction.x > 0: # se movendo para a direita
-                        self.hitbox.right = sprite.hitbox.left # a extrema direita do nosso Player coincide com a extrema esquerda do obstáculo que se está colidindo
-                    if self.direction.x < 0: # se movendo para a esquerda 
-                        self.hitbox.left = sprite.hitbox.right #a extrema esquerda do nosso Player coincide com a extrema direita do obstáculo que se está colidindo
+                    if self.direction.x > 0:  # se movendo para a direita
+                        # a extrema direita do nosso Player coincide com a extrema esquerda do obstáculo que se está colidindo
+                        self.hitbox.right = sprite.hitbox.left
+                    if self.direction.x < 0:  # se movendo para a esquerda
+                        # a extrema esquerda do nosso Player coincide com a extrema direita do obstáculo que se está colidindo
+                        self.hitbox.left = sprite.hitbox.right
 
         if direction == 'vertical':
             for sprite in self.obstacle_sprites:
                 if sprite.hitbox.colliderect(self.hitbox):
-                    if self.direction.y > 0: # se movendo para baixo
-                        self.hitbox.bottom = sprite.hitbox.top # a extrema inferior do nosso Player coincide com a extrema superior do obstáculo que se está colidindo
-                    if self.direction.y < 0: # se movendo para cima
-                        self.hitbox.top = sprite.hitbox.bottom # a extrema superior do nosso Player coincide com a extrema inferior do obstáculo que se está colidindo
+                    if self.direction.y > 0:  # se movendo para baixo
+                        # a extrema inferior do nosso Player coincide com a extrema superior do obstáculo que se está colidindo
+                        self.hitbox.bottom = sprite.hitbox.top
+                    if self.direction.y < 0:  # se movendo para cima
+                        # a extrema superior do nosso Player coincide com a extrema inferior do obstáculo que se está colidindo
+                        self.hitbox.top = sprite.hitbox.bottom
+
+    def cooldowns(self):
+        current_time = pygame.time.get_ticks()  # mede continuamente nosso tempo
+        if self.attacking:
+            if current_time - self.attack_time >= self.attack_cooldown:
+                self.attacking = False
+
+    def animate(self):
+        animation = self.animations[self.status]
+
+        # atualiza o índice da animação
+        self.frame_index += self.animation_speed
+        if self.frame_index >= len(animation):
+            self.frame_index = 0
+
+        # atualiza a imagem do player
+        self.image = animation[int(self.frame_index)]
+        self.rect = self.image.get_rect(center = self.hitbox.center)
 
     def update(self):
         self.input()
+        self.cooldowns()
+        self.get_status()
+        self.animate()
         self.move(self.speed)
