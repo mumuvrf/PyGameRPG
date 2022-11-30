9007ca0b22edede62c436ec99d9a56422bfe49df
Samuel Jabes <samueljcc@al.insper.edu.br>
2022-11-30 12:10:04 -0300

    Adiciona tela de início de jogo

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ class Game():
         # inicia pygame e cria janela, ou seja, gera tela principal
         pygame.init()
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.screen_rect = self.screen.get_rect()
         pygame.display.set_caption('VILLAGE WARRIOR')
         self.clock = pygame.time.Clock()
 
@@ -19,7 +20,38 @@ class Game():
         main_sound = pygame.mixer.Sound('audio/main.ogg')
         main_sound.play(loops = -1)
 
+        self.loop = True
+        self.window = pygame.image.load('assets/penes_de_fundo.png').convert_alpha()
+    
+    def init_screen(self):
+        while self.loop:
+            # ----- Trata eventos
+            for event in pygame.event.get():
+                # ----- Verifica consequências
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RETURN:
+                        state = GAME
+                        self.loop = False
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    state = QUIT
+
+            self.screen.blit(self.window, self.screen_rect)
+            pygame.display.update()
+                    
+        return state
+
     def run(self):
+        state = INIT
+        while state != QUIT:
+            if state == INIT:
+                state = self.init_screen()
+            elif state == GAME:
+                state = self.gaming()
+            else:
+                state = QUIT
+
+    def gaming(self):
         # ===== Loop principal =====
         while True:
             # ----- Trata eventos
